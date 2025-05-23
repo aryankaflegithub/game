8db5a45161a4a3c11be2266f714c0632a08aee8c
Aryan kafle <kaflearyan11@gmail.com>
2025-05-23 19:28:03 +0545

    walk limit

--- script/entity.py
+++ script/entity.py
@@ -169,11 +169,14 @@ class Enemy(PhysicsEntity):
         
     def update(self, tilemap, movement=(0, 0)):
         if self.walking:
-            movement = (movement[0] - 0.5 if self.flip else 0.5, movement[1])
+            if tilemap.solid_check((self.rect().centerx + (-5 if self.flip else 5), self.pos[1] + 23)):
+                movement = (movement[0] - 0.5 if self.flip else 0.5, movement[1])
+            else:
+                self.flip = not self.flip  
             self.walking = max(0, self.walking - 1)
         elif random.random() < 0.01:
             self.walking = random.randint(30, 120)
             
         super().update(tilemap, movement=movement)
             
-            
+             
