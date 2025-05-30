8fa274698b9f962b90de86089b5eb7b599385f5d
Aryan kafle <kaflearyan11@gmail.com>
2025-05-30 11:08:01 +0545

    haptics feedback

--- game.py
+++ game.py
@@ -51,6 +51,7 @@ class Game:
         
         self.tilemap = Tilemap(self, tile_size=16)
         self.load_level(0)
+        self.screenshake = 0
         
     def load_level(self, map_id):
         self.tilemap.load('data/maps/' + str(map_id) + '.json')
@@ -76,6 +77,7 @@ class Game:
     def run(self):
         while True:
             self.display.blit(self.assets['background'], (0, 0))
+            self.screenshake = max(0, self.screenshake - 1)
             
             if self.dead:
                 self.dead += 1
@@ -121,6 +123,7 @@ class Game:
                     if self.player.rect().collidepoint(projectile[0]):
                         self.projectiles.remove(projectile)
                         self.dead += 1
+                        self.screenshake = max(16, self.screenshake)
                         for i in range(30):
                             angle = random.random() * math.pi * 2
                             speed = random.random() * 5
@@ -161,9 +164,10 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-                        
             
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+                        
+            screenshake_offset = (random.random() * self.screenshake - self.screenshake / 2, random.random() * self.screenshake - self.screenshake / 2)
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), screenshake_offset)
             pygame.display.update()
             self.clock.tick(50)
 
